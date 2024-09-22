21f09a86b493be27ceeb2c82d6ab2fa0bc0160ce
NataliaKonoz <Nataliia.Konoz@cs.khpi.edu.ua>
2024-09-22 20:11:22 +0300

    all links added

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,17 +1,21 @@
-const btnBurgerOff = document.querySelector('#burger');
+const btnBurger = document.querySelector('#burger');
 const btnBurgerPackage = document.querySelector('#burger-package');
 const menu = document.querySelector('.header__section--mobile');
-const linkBurger = document.querySelector('.navigation__link--mobile');
+const linkBurger = document.querySelectorAll('.navigation__link--mobile');
 
-btnBurgerOff.addEventListener('click', () => {
+btnBurger.addEventListener('click', () => {
   menu.classList.toggle('open');
-  btnBurgerOff.classList.toggle('open');
+  btnBurger.classList.toggle('open');
 });
 
-linkBurger.addEventListener('click', () => {
-  menu.classList.remove('open');
+linkBurger.forEach(link => {
+  link.addEventListener('click', () => {
+    btnBurger.classList.remove('open');
+    menu.classList.remove('open');
+  });
 });
 
 btnBurgerPackage.addEventListener('click', () => {
   menu.classList.remove('open');
+  btnBurger.classList.remove('open');
 });
