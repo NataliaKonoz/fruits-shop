82edd8e07f69ebbe106d74294c85acc90bdbe836
NataliaKonoz <Nataliia.Konoz@cs.khpi.edu.ua>
2024-09-22 10:18:08 +0300

    add animations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const btnBurgerOff = document.querySelector('#burger-off');
+const btnBurgerPackage = document.querySelector('#burger-package');
 const header = document.querySelector('header');
+const linkBurger = document.querySelector('.navigation__link--mobile');
 
 btnBurgerOff.addEventListener('click', () => {
   header.classList.toggle('open');
+});
+
+linkBurger.addEventListener('click', () => {
+  header.classList.remove('open');
+});
+
+btnBurgerPackage.addEventListener('click', () => {
+  header.classList.remove('open');
 });
