ebf90efe500ce782f7ba477704d1c3d0926e15a3
NataliaKonoz <Nataliia.Konoz@cs.khpi.edu.ua>
2024-09-23 12:35:20 +0300

    implementation of the opening and closing of the form

--- js/form.js
+++ js/form.js
@@ -1,10 +1,29 @@
-const loadFormBtn = document.getElementById('orderBtn');
-const formContainer = document.querySelector('.form-container');
+const formContainer = document.getElementById('formContainer');
+const loadFormBtn = document.querySelectorAll('[id^="showFormBtn"]');
+const removeFormBtn = document.querySelector('#removeFormBtn');
 
-loadFormBtn.addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function () {
   fetch('/pages/order-form.html')
     .then(response => response.text())
     .then(data => {
-      formContainer.innerHTML = data;
+      formContainer.insertAdjacentHTML('beforeend', data);
     });
+});
+
+formContainer.classList.add('hidden');
+
+loadFormBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (formContainer.classList.contains('hidden')) {
+      formContainer.classList.remove('hidden');
+      formContainer.classList.add('active');
+    }
+  });
+});
+
+removeFormBtn.addEventListener('click', () => {
+  if (formContainer.classList.contains('active')) {
+    formContainer.classList.remove('active');
+    formContainer.classList.add('hidden');
+  }
 });
